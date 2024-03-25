135f6080ef5d03b69ab94c4a44dd8129202d0068
HenriS23 <henri.siepmann@code.berlin>
2024-03-25 22:26:37 +0100

    font fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,14 @@
-'use client'
+"use client"
 
-import { QueryClient, QueryClientProvider, useQuery } from 'react-query';
+import { QueryClient, QueryClientProvider } from "react-query"
+
 import "@/styles/globals.css"
-import { Metadata } from "next"
-
-import { siteConfig } from "@/config/site"
 import { fontSans } from "@/lib/fonts"
 import { cn } from "@/lib/utils"
+import { Toaster } from "@/components/ui/toaster"
 import { SiteHeader } from "@/components/site-header"
 import { TailwindIndicator } from "@/components/tailwind-indicator"
 import { ThemeProvider } from "@/components/theme-provider"
-import { Toaster } from "@/components/ui/toaster"
 
 // export const metadata: Metadata = {
 //   title: {
@@ -30,35 +28,38 @@ import { Toaster } from "@/components/ui/toaster"
 // }
 
 interface RootLayoutProps {
-  children: React.ReactNode
+    children: React.ReactNode
 }
 
 export default function RootLayout({ children }: RootLayoutProps) {
+    const queryClient = new QueryClient()
 
-  const queryClient = new QueryClient();
-
-  return (
-    <>
-    <QueryClientProvider client={queryClient}>
-      <html lang="en" suppressHydrationWarning>
-        <head />
-        <body
-          className={cn(
-            "min-h-screen bg-background font-sans antialiased",
-            fontSans.variable
-          )}
-        >
-          <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
-            <div className="relative flex min-h-screen flex-col">
-              <SiteHeader />
-              <div className="flex-1">{children}</div>
-            </div>
-            <TailwindIndicator />
-          </ThemeProvider>
-          <Toaster />
-        </body>
-      </html>
-      </QueryClientProvider>
-    </>
-  )
+    return (
+        <>
+            <QueryClientProvider client={queryClient}>
+                <html lang="en" suppressHydrationWarning>
+                    <head />
+                    <body
+                        className={cn(
+                            "min-h-screen bg-background font-sans antialiased",
+                            fontSans.variable
+                        )}
+                    >
+                        <ThemeProvider
+                            attribute="class"
+                            defaultTheme="system"
+                            enableSystem
+                        >
+                            <div className="relative flex min-h-screen flex-col">
+                                <SiteHeader />
+                                <div className="flex-1">{children}</div>
+                            </div>
+                            <TailwindIndicator />
+                        </ThemeProvider>
+                        <Toaster />
+                    </body>
+                </html>
+            </QueryClientProvider>
+        </>
+    )
 }
